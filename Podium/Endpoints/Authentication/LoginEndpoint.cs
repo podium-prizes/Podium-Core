8a3574bc228b6b415a71f66ee3b68a58a56559a9
iceixia <androidftw1994@gmail.com>
2019-04-09 22:31:26 +0100

    added login endpoint method

--- Podium/Endpoints/Authentication/LoginEndpoint.cs
+++ Podium/Endpoints/Authentication/LoginEndpoint.cs
@@ -1,7 +1,59 @@
+using System.Linq;
+
 namespace Podium.Endpoints.Authentication
 {
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.EntityFrameworkCore;
+    using Podium.Models.DbModels;
+    using Podium.Models.EndpointModels.Authentication;
+    using BCrypt.Net;
+ 
     public class LoginEndpoint
     {
-        
+        public static LoginEndpointModels.ResponseModel EndpointMethod(PodiumContext db,
+            LoginEndpointModels.RequestModel req)
+        {
+            var res = new LoginEndpointModels.ResponseModel();
+
+            try
+            {
+                var findUser = db.Users.Include(x => x.UserSessionList).SingleOrDefault(x =>
+                    x.Username == req.Username && BCrypt.Verify(req.Password, x.Password) && x.CanLogin);
+
+                if (findUser == null)
+                {
+                    throw new Exception("Login failed");
+                }
+                else
+                {
+                    res.Token = Guid.NewGuid().ToString().Replace("-", "");
+                    
+                    findUser.UserSessionList.Add(new UserSessionDbModel()
+                    {
+                        SessionToken = res.Token,
+                        IsValid = true,
+                        LoginDateTime = DateTime.Now,
+                        HasExpiry = false,
+                        HasExpired = false
+                    });
+
+                    db.SaveChanges();
+
+                    res.Message = "Login successful for user " + findUser.Username;
+                    res.Status = true;
+                }
+            }
+            catch (Exception e)
+            {
+                res = new LoginEndpointModels.ResponseModel()
+                {
+                    Message = e.Message,
+                    Status = false
+                };
+            }
+
+            return res;
+        }
     }
 }
